00ba23b59b70d53c98c3295ecca09e17cdc8dcc2
ezmhme <emzhemingway@gmail.com>
2024-02-29 21:16:46 +1300

    Update maeda_clock.js

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,85 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  background(0); //  beige
+  // fill(200); // dark grey
+  // textSize(400);
+  // textAlign(CENTER, CENTER);
+  // text("11", width/2, height/2);
+
+  noFill();
+  stroke(255,204,0);
+  strokeWeight(5);
+  circle(100,80,50);
+
+  noFill();
+  stroke(255,204,0);
+  strokeWeight(5);
+  circle(100,160,50);
+
+  noFill();
+  stroke(255,204,0);
+  strokeWeight(5);
+  circle(100,240,50);
+
+  noFill();
+  stroke(255,204,0);
+  strokeWeight(5);
+  circle(100,320,50);
+
+  noFill();
+  stroke(255,204,0);
+  strokeWeight(5);
+  circle(100,400,50);
+
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(5);
+  circle(100,80,30);
+
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(5);
+  circle(100,160,30);
+
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(5);
+  circle(100,240,30);
+
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(5);
+  circle(100,320,30);
+
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(5);
+  circle(100,400,30);
+
+  noFill();
+  stroke(0,0,255);
+  strokeWeight(5);
+  circle(100,400,70);
+
+  noFill();
+  stroke(0,0,255);
+  strokeWeight(5);
+  circle(100,320,70);
+
+  noFill();
+  stroke(0,0,255);
+  strokeWeight(5);
+  circle(100,240,70);
+
+  noFill();
+  stroke(0,0,255);
+  strokeWeight(5);
+  circle(100,160,70);
+
+  noFill();
+  stroke(0,0,255);
+  strokeWeight(5);
+  circle(100,80,70);
+
 }
