bf611e4c445fe23d4b8ac9b89e8008ea992a4f58
ezmhme <emzhemingway@gmail.com>
2024-03-07 10:19:12 +1300

    updates

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Update this file as you go along to record your progress.
 
 29/02
 Updated sketch.jpg with current stage of idea and began experimenting with maeda clock visuals. Coding is a bit rusty at this point but will reread old assignment codes to rejog my memory.
+![alt text](<Screenshot (234).png>)
+
+3/03
--- maeda_clock.js
+++ maeda_clock.js
@@ -2,84 +2,87 @@
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(0); //  beige
-  // fill(200); // dark grey
-  // textSize(400);
-  // textAlign(CENTER, CENTER);
-  // text("11", width/2, height/2);
+  fill(255); // dark grey
+  textSize(400);
+  textAlign(CENTER, CENTER);
+  text("9", width/4, height/2);
 
-  noFill();
-  stroke(255,204,0);
-  strokeWeight(5);
-  circle(100,80,50);
+  fill(100);
+  textSize(100);
+  text("49", width/1.5, height/2);
 
-  noFill();
-  stroke(255,204,0);
-  strokeWeight(5);
-  circle(100,160,50);
+  // noFill();
+  // stroke(255,204,0);
+  // strokeWeight(5);
+  // circle(100,80,50);
 
-  noFill();
-  stroke(255,204,0);
-  strokeWeight(5);
-  circle(100,240,50);
+  // noFill();
+  // stroke(255,204,0);
+  // strokeWeight(5);
+  // circle(100,160,50);
 
-  noFill();
-  stroke(255,204,0);
-  strokeWeight(5);
-  circle(100,320,50);
+  // noFill();
+  // stroke(255,204,0);
+  // strokeWeight(5);
+  // circle(100,240,50);
 
-  noFill();
-  stroke(255,204,0);
-  strokeWeight(5);
-  circle(100,400,50);
+  // noFill();
+  // stroke(255,204,0);
+  // strokeWeight(5);
+  // circle(100,320,50);
 
-  noFill();
-  stroke(255,0,0);
-  strokeWeight(5);
-  circle(100,80,30);
+  // noFill();
+  // stroke(255,204,0);
+  // strokeWeight(5);
+  // circle(100,400,50);
 
-  noFill();
-  stroke(255,0,0);
-  strokeWeight(5);
-  circle(100,160,30);
+  // noFill();
+  // stroke(255,0,0);
+  // strokeWeight(5);
+  // circle(100,80,30);
 
-  noFill();
-  stroke(255,0,0);
-  strokeWeight(5);
-  circle(100,240,30);
+  // noFill();
+  // stroke(255,0,0);
+  // strokeWeight(5);
+  // circle(100,160,30);
 
-  noFill();
-  stroke(255,0,0);
-  strokeWeight(5);
-  circle(100,320,30);
+  // noFill();
+  // stroke(255,0,0);
+  // strokeWeight(5);
+  // circle(100,240,30);
 
-  noFill();
-  stroke(255,0,0);
-  strokeWeight(5);
-  circle(100,400,30);
+  // noFill();
+  // stroke(255,0,0);
+  // strokeWeight(5);
+  // circle(100,320,30);
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(5);
-  circle(100,400,70);
+  // noFill();
+  // stroke(255,0,0);
+  // strokeWeight(5);
+  // circle(100,400,30);
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(5);
-  circle(100,320,70);
+  // noFill();
+  // stroke(0,0,255);
+  // strokeWeight(5);
+  // circle(100,400,70);
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(5);
-  circle(100,240,70);
+  // noFill();
+  // stroke(0,0,255);
+  // strokeWeight(5);
+  // circle(100,320,70);
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(5);
-  circle(100,160,70);
+  // noFill();
+  // stroke(0,0,255);
+  // strokeWeight(5);
+  // circle(100,240,70);
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(5);
-  circle(100,80,70);
+  // noFill();
+  // stroke(0,0,255);
+  // strokeWeight(5);
+  // circle(100,160,70);
 
+  // noFill();
+  // stroke(0,0,255);
+  // strokeWeight(5);
+  // circle(100,80,70);
 }
